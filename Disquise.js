93c3603aa5236491ba89d339bd760bc0f298eb75
theBlaize <98980660+theBlaize@users.noreply.github.com>
2022-05-17 16:12:27 -0600

    Create Disquise.js

---
+++ Disquise.js
@@ -0,0 +1,1 @@
+javascript:(function() {    var link = document.querySelector("link[rel*='icon']") || document.createElement('link');    link.type = 'image/x-icon';    link.rel = 'shortcut icon';    link.href = 'https://ssl.gstatic.com/classroom/favicon.png';    document.title='Classroom';    console.log(document.title);    document.getElementsByTagName('head')[0].appendChild(link);})();
